44931c9917217725abc7ce8a5ea3ff6b82f92852
MotoWiZ <motowiz@hotmail.com>
2022-11-26 21:25:13 +0000

    3 Windows with Original, reduced and mask view

--- ball.py
+++ ball.py
@@ -2,11 +2,6 @@ import cv2
 import time
 from picamera2 import Picamera2 
 
-# Define CV2 Windows
-cv2.namedWindow("Original image", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Masked image", cv2.WINDOW_NORMAL)
-
-cX, cY = 0, 0
 lower_white = (0, 0, 0)
 upper_white = (0, 0, 255)
 t = time.time()
@@ -18,16 +13,25 @@ def camera_config():
     picam2.start()
     return picam2
 
+# Configure ROI with double mouse clicks
+def mark_roi(event,x,y,flags,param):
+  global mouseX, mouseY, nr_click
+  if event == cv2.EVENT_LBUTTONDBLCLK:
+      cv2.circle(img,(x,y),10,(0, 0, 255),-1)
+      mouseX, mouseY = x, y
+      nr_click += 1
+
 # Crop and convert image from camera    
 def get_image(picam2):
     img = picam2.capture_array()
-    hsv = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
     img = cv2.cvtColor(img,cv2.COLOR_RGBA2RGB)
     
-    return img, hsv
+    return img
 
 # Find white ball on table
-def find_ball(img):
+def find_ball(img, x_cent, y_cent):
+    cX, cY = 0, 0
+    hsv = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
     # Mask image for whites
     mask = cv2.inRange(hsv, lower_white, upper_white)
     
@@ -40,39 +44,94 @@ def find_ball(img):
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
     
-    # Place red circle in center of white ball
+    # Place red circle and text in center of white ball
     cv2.circle(result, (cX, cY), 10, (0, 0, 255), -1)
-    # Place green circle and text in center of result
-    cv2.circle(result, (400, 300), 10, (0, 255, 0), -1)
     cv2.putText(result, "Object center", (cX - 110, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
-    # Place green circle in center of original image
-    cv2.circle(img, (400, 300), 10, (0, 255, 0), -1)
+    # Place green circle in center of result
+    cv2.circle(result, (x_cent, y_cent), 10, (0, 255, 0), -1)
+    # Place green circle in center of reduced image
+    cv2.circle(img, (x_cent, y_cent), 10, (0, 255, 0), -1)
     
+  
     return img, result
 
-def windows(img, result):
+def windows(original, img, result):
+
     # Original image window
-    cv2.imshow("Original image",img)
+    cv2.namedWindow("Original image", cv2.WINDOW_NORMAL)
+    cv2.imshow("Original image",original)
     cv2.resizeWindow("Original image", 400, 300)
     cv2.moveWindow("Original image", 10,100)
     
+    # Reduced image window
+    cv2.namedWindow("Reduced image", cv2.WINDOW_NORMAL)
+    cv2.imshow("Reduced image",img)
+    cv2.resizeWindow("Reduced image", 400, 300)
+    cv2.moveWindow("Reduced image", 420,100)
+    
     # Masked image window
+    cv2.namedWindow("Masked image", cv2.WINDOW_NORMAL)
     cv2.imshow("Masked image",result)
     cv2.resizeWindow("Masked image", 400, 300) 
-    cv2.moveWindow("Masked image", 440,100) 
+    cv2.moveWindow("Masked image", 840,100) 
 
 if __name__ == "__main__":
     picam2 = camera_config()
+    img = get_image(picam2)
+    cv2.namedWindow("Pick points")
+    cv2.moveWindow("Pick points", 320,100) 
+    cv2.setMouseCallback("Pick points",mark_roi)
+    nr_click = 0
+    prev_nr_click = nr_click
+    x_pos, y_pos = [], []
+    
+    while True:
+        cv2.imshow("Pick points",img)
+        k = cv2.waitKey(1) & 0xFF
+        if nr_click == 3 or k == ord('q'):
+            break
+        elif nr_click != prev_nr_click:
+            prev_nr_click = nr_click
+            x_pos.append(mouseX)
+            y_pos.append(mouseY)
+            #print (mouseX,mouseY,nr_click)
+    cv2.destroyAllWindows()
+    
+    #print (x_pos[0], y_pos[0])
+    #print (x_pos[1], y_pos[1])
     
     while True:
         # Start timer to count FPS
         start_time = time.time()
         
-        img, hsv = get_image(picam2)
+        original = get_image(picam2)
+        img = original.copy()
         
-        img, result = find_ball(img)
+        if y_pos[0] < y_pos[1]:
+            y_cent = int((y_pos[1] - y_pos[0])/2)
+            if x_pos[0] < x_pos[1]:
+                img = img[y_pos[0]:y_pos[1], x_pos[0]:x_pos[1]]
+                x_cent = int((x_pos[1] - x_pos[0])/2)
+            elif x_pos[0] > x_pos[1]:
+                img = img[y_pos[0]:y_pos[1], x_pos[1]:x_pos[0]]
+                x_cent = int((x_pos[0] - x_pos[1])/2)
+        elif y_pos[0] > y_pos[1]:
+            y_cent = int((y_pos[0] - y_pos[1])/2)
+            if x_pos[0] < x_pos[1]:
+                img = img[y_pos[1]:y_pos[0], x_pos[0]:x_pos[1]]
+                x_cent = int((x_pos[1] - x_pos[0])/2)
+            elif x_pos[0] > x_pos[1]:
+                img = img[y_pos[1]:y_pos[0], x_pos[1]:x_pos[0]]
+                x_cent = int((x_pos[0] - x_pos[1])/2)
+
+        #print(x_cent, y_cent)
         
-        windows(img, result)        
+        img, result = find_ball(img, x_cent, y_cent)
+        
+        # Place green circle in center of original image
+        cv2.circle(original, (400, 300), 10, (0, 255, 0), -1)
+        
+        windows(original, img, result)        
 
         # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'): 
